3fcdc2e2d41723a51c75723ceeae1fe3f63176fe
SmutnyJan <smutnyjan2003@seznam.cz>
2025-03-28 18:19:19 +0100

    cron test

---
+++ CobraApi/UpdateDataFunction/UpdateStockDataFunction.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+public class UpdateStockDataFunction
+{
+    private readonly ILogger _logger;
+    private static readonly HttpClient client = new HttpClient();
+
+    public UpdateStockDataFunction(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<UpdateStockDataFunction>();
+    }
+
+    [Function("UpdateStockDataFunction")]
+    public async Task Run([TimerTrigger("0 0 */6 * * *")] object _)
+    {
+        _logger.LogInformation($"Running stock update at: {DateTime.Now}");
+
+        var response = await client.PostAsync("https://cobraapiapi.azure-api.net/api/StockDatas/UpdateCurrentPrices", null);
+
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("Stock prices updated successfully.");
+        }
+        else
+        {
+            _logger.LogError($"Error updating stock prices: {response.StatusCode}");
+        }
+    }
+}
